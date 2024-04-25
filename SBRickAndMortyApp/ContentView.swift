6c01eb79d8d315be820df262147e1d78175929e2
SergioBecerril <sbecerril@cfeapps.com>
2024-04-25 13:15:45 +0200

    Upload project to origin

---
+++ SBRickAndMortyApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SBRickAndMortyApp
+//
+//  Created by Sergio Becerril on 25/04/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello Rick and Morty")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SBRickAndMortyApp/SBRickAndMortyAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SBRickAndMortyAppApp.swift
+//  SBRickAndMortyApp
+//
+//  Created by Sergio Becerril on 25/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct SBRickAndMortyAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
