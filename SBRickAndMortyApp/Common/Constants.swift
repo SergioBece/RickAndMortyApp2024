168afa8e66865498bd5fd659160d6d3146defa4c
SergioBecerril <sbecerril@cfeapps.com>
2024-04-25 16:26:35 +0200

    create constants file

---
+++ SBRickAndMortyApp/Common/Constants.swift
@@ -0,0 +1,20 @@
+//
+//  Constants.swift
+//  SBRickAndMortyApp
+//
+//  Created by Sergio Becerril on 25/04/2024.
+//
+
+import Foundation
+
+public struct Constants {
+    
+    public struct Literals {
+        static let charactersTitle = "Personajes"
+    }
+    
+    public struct SystemImage {
+        static let settingsButton = "gear"
+    }
+    
+}
