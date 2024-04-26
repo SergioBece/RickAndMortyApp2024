dc93e952d8eb66a7f11a9fccfc5acddcbad078e4
SergioBecerril <sbecerril@cfeapps.com>
2024-04-26 12:13:35 +0200

    refactor and added code

--- SBRickAndMortyApp/Modules/Characters/Character/View/CharacterView.swift
+++ SBRickAndMortyApp/Modules/Characters/Character/View/CharacterView.swift
@@ -20,28 +20,65 @@ struct CharacterView: View {
     ]
     
     var body: some View {
-        NavigationView {
+        NavigationStack {
             ScrollView {
                 LazyVGrid(columns: guides) {
                     ForEach(viewModel.characters) { character in
-                        //TODO: Implement View
+                        NavigationLink(value: character) {
+                            ZStack(alignment: .bottomLeading) {
+                                AsyncImage(url: URL(string: character.image), content: { image in
+                                    image
+                                        .resizable()
+                                        .modifier(CharacterImage(radius: 10))
+                                }, placeholder: {
+                                    Image(.clock)
+                                        .resizable()
+                                        .modifier(CharacterImage(radius: 10))
+                                })
+                                
+                                Text(character.name)
+                                    .foregroundStyle(.white)
+                                    .font(.caption)
+                                    .padding(5)
+                                    .background(Color.black.opacity(0.7))
+                                    .cornerRadius(8)
+                                    .padding(3)
+                            }
+                        }
+                        .onAppear {
+                            // Detectar cuándo se acerca el final de la lista para recargar la siguiente página
+                            if character == viewModel.characters.last {
+                                currentPage += 1
+                                guard let totalPages = viewModel.dataCharacter?.info.pages else { return }
+                                if currentPage <= totalPages {
+                                    viewModel.getCharacters(page: currentPage)
+                                }
+                            }
+                        }
                     }
                 }
-                //TODO: Implement Task for get Characters
+                
+                .task {
+                    viewModel.getCharacters(page: currentPage)
+                }
+                
             }
-            //TODO: Navigate to DetailView
+            .navigationDestination(for: CharacterWithEpisode.self, destination: { characterSelected in
+                DetailView(characterSelected: characterSelected)
+            })
             .navigationTitle(Constants.Literals.charactersTitle)
             .toolbar {
                 ToolbarItemGroup(placement: .primaryAction) {
                     Button(action: {
                         isViewPresented = true
                     }, label: {
-                        Image(systemName: Constants.SystemImage.settingsButton)
+                        Image(systemName: Constants.systemImage.settingsButton)
                     })
                 }
             }
             .sheet(isPresented: $isViewPresented, content: {
-                //TODO: Navigate to Settings View
+                SettingsView(isViewPresented: $isViewPresented, darkMode: $darkMode)
+                    .presentationDetents([.medium])
             })
         }
     }
